bd16a91fe835a35999cc9d7f61c475632e932d2d
Sergey Chikuyonok <serge.che@gmail.com>
2019-01-30 10:40:34 +0300

    Added ESLint for generated output

--- test/template-generator.ts
+++ test/template-generator.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as assert from 'assert';
+import { Linter } from 'eslint';
 import { compile } from '../index';
 
 describe('Template generator', () => {
@@ -12,12 +13,24 @@ describe('Template generator', () => {
         const samples = path.resolve(__dirname, 'samples/templates');
         const fixtures = path.resolve(__dirname, 'fixtures/templates');
         const files = fs.readdirSync(samples);
+        const linter = new Linter();
+        const linterConfig = require('./fixtures/templates/.eslintrc.js');
 
         files.forEach(file => {
             const template = read(path.join(samples, file));
             const fixture = read(path.join(fixtures, file.replace(/\.\w+$/, '.js')));
             const output = compile(template, file);
-            assert.equal(output.code.trim(), fixture, file);
+            const code = output.code.trim();
+            assert.equal(code, fixture, file);
+
+            const errors = linter.verify(code, linterConfig, { filename: file })
+                .filter(item => item.fatal)
+                .map(item => `${item.message} at line ${item.line}, column ${item.column}`)
+                .join('\n');
+
+            if (errors) {
+                throw new Error(`Lint errors in ${file}:\n\n${errors}`);
+            }
         });
     });
 });
